80b09aceb1c81e236824b9b7a180809805c33cde
Oscarqjh <aerialguard@gmail.com>
2024-10-14 22:03:37 +0200

    move and positions

--- NoGarbageInTheCans.cc
+++ NoGarbageInTheCans.cc
@@ -15,7 +15,7 @@ Define_Module(CanNode);
 void CanNode::initialize()
 {
     cMessage *msg = new cMessage("I am Can Node");
-    bubble("I am can node");
+    send(msg, "out", 0);
 }
 
 void CanNode::handleMessage(cMessage *msg)
@@ -32,6 +32,8 @@ class TruckNode : public cSimpleModule
     protected:
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
+    private:
+        cMessage *msgDelay;
 };
 
 Define_Module(TruckNode);
@@ -39,13 +41,28 @@ Define_Module(TruckNode);
 void TruckNode::initialize()
 {
     cMessage *msg = new cMessage("I am Truck Node");
-    bubble("I am truck node");
+    send(msg, "out", 0);
+    msgDelay = new cMessage("msgDelay");
+    scheduleAt(simTime() + 4.0, msgDelay);
+
 }
 
 void TruckNode::handleMessage(cMessage *msg)
 {
     bubble("received");
+
+    if (msg == msgDelay) {
+        // This block is executed when the delayed message is triggered
+        EV << "Message sent after delay!" << endl;
+        bubble("hi");
+
+        // Send the message through the specified output gate by index
+        send(msg, "out"); // "out" is the base name of the gates, targetGateIndex is the gate index
+    } else {
+        // Handle other messages here if needed
+    }
 }
+
 
 
 ///////////////////////////////////////////////////////////////
@@ -63,7 +80,6 @@ Define_Module(CloudNode);
 void CloudNode::initialize()
 {
     cMessage *msg = new cMessage("I am Cloud Node");
-    bubble("I am Cloud node");
 }
 
 void CloudNode::handleMessage(cMessage *msg)
@@ -86,7 +102,6 @@ Define_Module(VisualizerNode);
 void VisualizerNode::initialize()
 {
     cMessage *msg = new cMessage("I am Visualizer Node");
-    bubble("I am Visualizer node");
 }
 
 void VisualizerNode::handleMessage(cMessage *msg)
